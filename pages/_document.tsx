a4cb5dc5ce07b5d386a24b25a2c8d3356f23225c
Petro Silenius <petro.silenius@gmail.com>
2021-12-06 11:00:02 +0200

    Add google api script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <script src="https://apis.google.com/js/api.js" async defer type="text/javascript" />
         </Head>
         <body>
           <Main />
