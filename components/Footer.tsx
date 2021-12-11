d26e072e4a05f92064c04a5a143e79f2c19e81e8
Petro Silenius <petro.silenius@gmail.com>
2021-12-12 00:19:57 +0200

    Show links to privacy policy and terms of service in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,39 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { css } from '@emotion/react';
+import { Grid, Link as MuiLink, Typography } from '@mui/material';
 
 export const Footer = () => {
   return (
-    <footer
-      css={css`
-        text-align: center;
-      `}>
-      Powered by{' '}
-      <a
-        href="https://silenius.dev"
-        css={css`
-          vertical-align: middle;
-          margin-left: 5px;
-        `}>
-        <Image src="/profileImg.png" alt="Vercel Logo" width={32} height={32} />
-      </a>
+    <footer>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={3}>
+          <Link href="/privacy" passHref>
+            <MuiLink>Privacy Policy</MuiLink>
+          </Link>
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          css={css`
+            text-align: center;
+          `}>
+          <Typography component="span">Powered by </Typography>
+          <MuiLink
+            href="https://silenius.dev"
+            css={css`
+              vertical-align: middle;
+              margin-left: 5px;
+            `}>
+            <Image src="/profileImg.png" alt="Vercel Logo" width={32} height={32} />
+          </MuiLink>
+        </Grid>
+        <Grid item xs={3}>
+          <Link href="/terms" passHref>
+            <MuiLink>Terms &amp; Conditions</MuiLink>
+          </Link>
+        </Grid>
+      </Grid>
     </footer>
   );
 };
