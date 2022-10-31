a511a64a0ab9d0269ed7a22860f21f206e53161d
Petro Silenius <petro.silenius@gmail.com>
2022-10-31 21:27:32 +0200

    Set font display to swap and configure fallback fonts

--- theme.ts
+++ theme.ts
@@ -1,7 +1,12 @@
 import { createTheme } from '@mui/material/styles';
 import { Roboto } from '@next/font/google';
 
-export const roboto = Roboto({ weight: '400', subsets: ['latin'] });
+export const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
 
 const theme = createTheme({
   palette: {
