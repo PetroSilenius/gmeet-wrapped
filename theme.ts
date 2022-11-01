31a7714cb088d2dfd1664bca1bb2ccc97d9a005e
Petro Silenius <petro.silenius@gmail.com>
2022-11-01 21:07:48 +0200

    Upgrade to next 13.1, use multiple font weights

--- theme.ts
+++ theme.ts
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material/styles';
 import { Roboto } from '@next/font/google';
 
 export const roboto = Roboto({
-  weight: '400',
+  weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
   display: 'swap',
   fallback: ['Helvetica', 'Arial', 'sans-serif'],
